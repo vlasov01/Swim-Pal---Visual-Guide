7006e731b4f7009e116069aa1df1c3e045b80d9c
Sergey Vlasov <vlasov01@gmail.com>
2023-11-30 10:52:40 -0500

    new file:   monitor.py

---
+++ monitor.py
@@ -0,0 +1,18 @@
+import pyttsx3
+
+#init text to speech
+engine = pyttsx3.init()
+
+#detect the end of lane
+#measure the distance to the end of lane
+
+#loop
+#detect the swimer
+#measure the distance to the swimer
+#detect swimming direction
+
+#calculate the distance to the end of lane
+
+#send notification if close to the end of lane
+engine.say("End of lane in 1 meter")
+engine.runAndWait()
